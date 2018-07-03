9c261c79d7e3d111ec3fe655eee50a402d5cd33e
user1 <user1@user>
2018-07-03 13:25:03 +0900

    Commit

---
+++ sp4-chap02/src/main/java/chap02/Greeter.java
@@ -0,0 +1,13 @@
+package chap02;
+
+public class Greeter {
+	private String format;
+	
+	public void setFormat(String format) {
+		this.format=format;
+	}	
+	
+	public String greet(String guest) {
+		return String.format(format, guest);
+	}
+}
