c54c9584a1e900858fcad3a75332e93695d5a5ef
user1 <user1@user>
2018-07-05 13:47:23 +0900

    Introduce about 'AOP'

---
+++ sp4-chap07/src/main/java/main/MainXmlPojo.java
@@ -0,0 +1,24 @@
+package main;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import chap07.Calculator;
+import chap07.ImpeCalculator;
+
+public class MainXmlPojo {
+	
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = 
+				new GenericXmlApplicationContext("classpath:aopPojo.xml");
+		
+		//Calculator impeCal = ctx.getBean("impeCal", Calculator.class);
+		
+		ImpeCalculator impeCal = ctx.getBean("impeCal", ImpeCalculator.class);
+		long fiveFact1 = impeCal.factorial(5);
+		System.out.println("impeCal.factorial(5)="+fiveFact1);
+		
+		Calculator recCal = ctx.getBean("recCal",Calculator.class);
+		long fiveFact2 = recCal.factorial(5);
+		System.out.println("recCal.factorial(5)="+fiveFact2);
+	}
+}
