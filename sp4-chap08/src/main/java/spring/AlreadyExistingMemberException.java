289b9f9f0c6b10c2cc32c910d35b94207c0dc5cd
user1 <user1@user>
2018-07-06 13:02:51 +0900

    Connection to DB

---
+++ sp4-chap08/src/main/java/spring/AlreadyExistingMemberException.java
@@ -0,0 +1,9 @@
+package spring;
+
+public class AlreadyExistingMemberException extends RuntimeException{
+
+	public AlreadyExistingMemberException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
